2c7e3bbbf66511944ea30de07918300cf54e11f9
undergraduate <cse20221124@cspro2.sogang.ac.kr>
2024-04-10 14:22:17 +0000

    after pattern matching

--- CMinus/src/CMinus.fs
+++ CMinus/src/CMinus.fs
@@ -9,30 +9,30 @@ let rec evalExp (exp: Exp) (mem: Mem) : Val =
   | Num i -> Int i
   | True -> Bool true
   | False -> Bool false
-  | Var s -> VarName string 
-  
+  | Var s -> s 
+
   | Add (x,y) -> 
     match evalExp x mem, evalExp y mem with
     | Int i1, Int i2 -> Int (i1+i2)
-    | _ -> printfn "boolean value can't be calculated!"
+    | _ -> Int (-1)
 
   | Sub (x,y) ->
     match evalExp x mem, evalExp y mem with
     | Int i1, Int i2 -> Int (i1-i2)
-    | _ -> printfn "boolean value can't be calculated!"
+    | _ -> Int (-1)
 
   | LessThan (x,y) -> 
     match evalExp x mem, evalExp y mem with
-    | Int i1, Int i2 -> if (i1 < i2) then true
-    | _ -> printfn "boolean value can't be calculated!"
+    | Int i1, Int i2 -> Bool (i1 < i2)
+    | _ -> Bool false
   
-  | GraterThan (x,y) ->
+  | GreaterThan (x,y) ->
     match evalExp x mem, evalExp y mem with
-    | Int i1, Int i2 -> if (i1 > i2) then true
-    | _ -> printfn "boolean value can't be calculated!"
+    | Int i1, Int i2 -> Bool (i1 > i2)
+    | _ -> Int (-1)
 
-  | Equal (x,y) -> if (evalExp x mem == evalExp y mem) then true 
-  | NotEz (x,y) -> if (evalExp x mem != evalExp y mem) then true 
+  | Equal (x,y) -> Bool (evalExp x mem = evalExp y mem)
+  | NotEq (x,y) -> Bool (evalExp x mem <> evalExp y mem)
   | _ -> Int (-1) // TODO: fill in the remaining cases. 
 
 // Note: You may define more functions.
@@ -50,15 +50,18 @@ let rec exec (stmt: Stmt) (mem: Mem) : Mem =
       let tf = evalExp e mem
       let mem_t = exec s1 mem
       let mem_f = exec s2 mem
-      if tf then mem_t else mem_f
+      match tf with
+      | Bool b -> if (b) then mem_t else mem_f
   | While (e,s) -> 
       let tf = evalExp e mem
-      if tf then
-        let m1 = exec s mem
-        let m2 = exec (While (e s)) (exec s mem) 
-        m2
-      else 
-        mem
+      match tf with
+      | Bool b -> 
+        if (b) then
+          let m1 = exec s mem
+          let m2 = exec (While (e,s)) (exec s mem) 
+          m2
+        else
+          mem
   | _ -> raise UndefinedSemantics // TODO: fill in the remaining cases.
 
 // The program starts execution with an empty memory. Do NOT fix this function.
